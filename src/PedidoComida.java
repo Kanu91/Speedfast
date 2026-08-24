79c53c81d824ff55e87cbef6199c9174f9bd317e
Kanu91 <131217105+Kanu91@users.noreply.github.com>
2026-08-23 21:49:02 -0400

    Update PedidoComida.java
    Implementación cálculo de entrega PedidoComida - Semana 2

--- src/PedidoComida.java
+++ src/PedidoComida.java
@@ -1,21 +1,11 @@
 public class PedidoComida extends Pedido {
 
-    public PedidoComida(String idPedido, String direccionEntrega) {
-        super(idPedido, direccionEntrega, "Comida");
+    public PedidoComida(String idPedido, String direccionEntrega, int distanciaKm) {
+        super(idPedido, direccionEntrega, distanciaKm);
     }
 
     @Override
-    public void asignarRepartidor() {
-        System.out.println("[Pedido Comida]");
-        System.out.println("Asignando repartidor...");
-        System.out.println("Verificando mochila térmica... OK");
-    }
-
-    @Override
-    public void asignarRepartidor(String nombreRepartidor) {
-        System.out.println("[Pedido Comida]");
-        System.out.println("Asignando repartidor...");
-        System.out.println("Verificando mochila térmica... OK");
-        System.out.println("Pedido asignado a " + nombreRepartidor);
+    public int calcularTiempoEntrega() {
+        return 15 + (2 * getDistanciaKm());
     }
 }
