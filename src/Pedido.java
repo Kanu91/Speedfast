d05002c887a09454ed09a232fbb6e590cd36c912
Kanu91 <131217105+Kanu91@users.noreply.github.com>
2026-08-23 21:46:20 -0400

    Update Pedido.java
    Actualización de Pedido a clase abstracta - Semana 2

--- src/Pedido.java
+++ src/Pedido.java
@@ -1,13 +1,13 @@
-public class Pedido {
+public abstract class Pedido {
 
     private String idPedido;
     private String direccionEntrega;
-    private String tipoPedido;
+    private int distanciaKm;
 
-    public Pedido(String idPedido, String direccionEntrega, String tipoPedido) {
+    public Pedido(String idPedido, String direccionEntrega, int distanciaKm) {
         this.idPedido = idPedido;
         this.direccionEntrega = direccionEntrega;
-        this.tipoPedido = tipoPedido;
+        this.distanciaKm = distanciaKm;
     }
 
     public String getIdPedido() {
@@ -18,15 +18,15 @@ public class Pedido {
         return direccionEntrega;
     }
 
-    public String getTipoPedido() {
-        return tipoPedido;
+    public int getDistanciaKm() {
+        return distanciaKm;
     }
 
-    public void asignarRepartidor() {
-        System.out.println("Asignando repartidor al pedido...");
+    public void mostrarResumen() {
+        System.out.println(getClass().getSimpleName() + " #" + idPedido);
+        System.out.println("Dirección: " + direccionEntrega);
+        System.out.println("Distancia: " + distanciaKm + " km");
     }
 
-    public void asignarRepartidor(String nombreRepartidor) {
-        System.out.println("Pedido asignado a: " + nombreRepartidor);
-    }
+    public abstract int calcularTiempoEntrega();
 }
