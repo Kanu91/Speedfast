4f0275c24f642bc2f5f8ff5b0e9b972ef16c9180
claudio_azocar <131217105+Kanu91@users.noreply.github.com>
2026-09-14 21:36:17 -0300

    Create Pedido.java
    Implementación clase Pedido - Semana 5

---
+++ semana 4/src/cl/duoc/speedfast/modelo/semana 5/src/cl/duoc/speedfast/modelo/Pedido.java
@@ -0,0 +1,49 @@
+package cl.duoc.speedfast.modelo;
+
+public class Pedido {
+
+    private int id;
+    private String direccionEntrega;
+    private EstadoPedido estado;
+
+    public Pedido(int id, String direccionEntrega) {
+        this.id = id;
+        this.direccionEntrega = direccionEntrega;
+        this.estado = EstadoPedido.PENDIENTE;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDireccionEntrega() {
+        return direccionEntrega;
+    }
+
+    public void setDireccionEntrega(String direccionEntrega) {
+        this.direccionEntrega = direccionEntrega;
+    }
+
+    public EstadoPedido getEstado() {
+        return estado;
+    }
+
+    public void setEstado(EstadoPedido estado) {
+        this.estado = estado;
+    }
+
+    public void setEstado(String nuevoEstado) {
+        this.estado = EstadoPedido.valueOf(nuevoEstado);
+    }
+
+    @Override
+    public String toString() {
+        return "Pedido #" + id
+                + " | Dirección: " + direccionEntrega
+                + " | Estado: " + estado;
+    }
+}
