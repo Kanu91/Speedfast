3e0fe971e635cf49bf168b8b021005edcfcb80f6
claudio_azocar <131217105+Kanu91@users.noreply.github.com>
2026-09-14 21:56:54 -0300

    Create ZonaDeCarga.java
    Implementación ZonaDeCarga sincronizada - Semana 5

---
+++ semana 5/src/cl/duoc/speedfast/concurrencia/ZonaDeCarga.java
@@ -0,0 +1,46 @@
+package cl.duoc.speedfast.concurrencia;
+
+import cl.duoc.speedfast.modelo.EstadoPedido;
+import cl.duoc.speedfast.modelo.Pedido;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ZonaDeCarga {
+
+    private final List<Pedido> pedidos;
+
+    public ZonaDeCarga() {
+        pedidos = new ArrayList<>();
+        System.out.println("[Zona de carga inicializada]");
+    }
+
+    public synchronized void agregarPedido(Pedido pedido) {
+        pedidos.add(pedido);
+
+        System.out.println(
+                "Pedido #" + pedido.getId()
+                        + " agregado. Destino: "
+                        + pedido.getDireccionEntrega()
+        );
+    }
+
+    public synchronized Pedido retirarPedido() {
+
+        for (int i = 0; i < pedidos.size(); i++) {
+
+            Pedido pedido = pedidos.get(i);
+
+            if (pedido.getEstado() == EstadoPedido.PENDIENTE) {
+                pedidos.remove(i);
+                return pedido;
+            }
+        }
+
+        return null;
+    }
+
+    public synchronized boolean estaVacia() {
+        return pedidos.isEmpty();
+    }
+}
